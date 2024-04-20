7446d21becae6a42586d1684ac261b8dae8fadc5
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-20 14:52:08 +0200

    chore: CI/CD

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:3000',
+    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://module-production.vercel.app/',
     trace: 'on-first-retry',
   },
   projects: [
